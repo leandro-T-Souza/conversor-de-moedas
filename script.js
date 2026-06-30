20baae14269ba056e52ddf301d860677040ffba3
Leandro-T-Souza <leandropop9@gmail.com>
2026-06-30 19:06:30 -0300

    Adicionei uma API

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-
-function convertValues() {
+async function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // valor em Real á Converter
     const currencyValueToConverted = document.querySelector(".currency-value") // Outras moedas já Convertida
 
-    const dolarToday = 5.2
-    const euroToday = 6.2
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolarToday = data.USDBRL.high
+    const euroToday = data.EURBRL.high
     const libraToday = 7.2
-    const bitcoinToday = 8.2
+    const bitcoinToday = data.BTCBRL.high
 
     if (currencySelect.value == "dolar") {
         // se select estiver selecionado o valor de dolar, entre aqui
